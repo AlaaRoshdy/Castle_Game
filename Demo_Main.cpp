eccf2e46fb65917e43153becb9e2f545df3ac73a
MohamedMahrous <s-mohamed.adham@zewailcity.edu.eg>
2018-11-27 18:01:50 +0200

    Deleted the jargin

--- Demo_Main.cpp
+++ Demo_Main.cpp
@@ -5,8 +5,6 @@ int main()
 {
 	Battle* pGameBattle = new Battle;
 	pGameBattle->RunSimulation();
-	//This is alaa's first commit 
-	// DS is both cool and not cool
 	delete pGameBattle;
 	
 	return 0;
